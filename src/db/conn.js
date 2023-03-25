30c2c3fa048bcd5ae416a3f2577146f7681da04f
khyati81 <67822169+khyati81@users.noreply.github.com>
2023-03-25 19:13:19 +0530

    error

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/clothing").then(()=> {
+mongoose.connect("mongodb://0.0.0.0:27017/clothing").then(()=> {
     console.log("Connection successful");
 }).catch((e)=>{
     console.log("No connection"+e);
