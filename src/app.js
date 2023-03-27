6cc82b7860ca927e0e359bcd289149bd74713383
khyati81 <67822169+khyati81@users.noreply.github.com>
2023-03-27 16:00:01 +0530

    cors2

--- src/app.js
+++ src/app.js
@@ -10,9 +10,13 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(router);
 
-var cors = require('cors')
-
-app.use(cors())
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 const start = async() =>{
     try{
